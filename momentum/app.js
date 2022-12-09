2fd7bd037c23b0d3ab8f9cfadf6bff105d7f8191
Jeck-Lee <jecklee02@gmail.com>
2022-12-09 11:38:30 +0900

    object

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,17 @@
-const a = 5;
-let b = 1;
-// 기존 js 문법은 모두 var를 사용 -> const: 상수 & let: 변수, 로 업데이트됨
-b = 2
+// Object
+const player = {
+    name: "Jeck",
+    points: 10,
+    fat: true,
+};
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
+console.log(player);
+console.log(player.name);
+
+// 상수로 선언한 객제 내 변수는 변경 가능
+player.points = 20;
+console.log(player);
+
+// 코드 상에서 객체의 프로퍼티 추가 가능
+player.lastName = "Family"
+console.log(player);
