05216e07fce3c0ba22c6c6dbbf43ca692db10790
Jeck-Lee <jecklee02@gmail.com>
2022-12-14 16:07:42 +0900

    CSS class in JavaScript

--- momentum/app.js
+++ momentum/app.js
@@ -1,28 +1,16 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    console.log("title was clicked!");
-    h1.style.color = "blue";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-    alert("Copier!");
-}
-function handleWindowOffline() {
-    alert("no wifi");
-}
-function handleWindowOnline() {
-    alert("!!--wifi--!!");
+    const activeClass = "active"
+    if (h1.className === activeClass) {
+        h1.className = "";
+    }
+    else {
+        // CSS 파일에서 선언한 클래스 이름과 같아야 함
+        h1.className = activeClass;
+    }
 }
 
 // title.onclick = handleTitleClick; 와 동일
 // addEventListener를 사용할 경우, removeEventListener로 삭제도 가능
 h1.addEventListener("click", handleTitleClick);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
