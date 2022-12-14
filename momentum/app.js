16314f33cac05905d9533266e935f88896641c5d
Jeck-Lee <jecklee02@gmail.com>
2022-12-14 15:53:44 +0900

    window events
    resize, copy, offline/online

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,28 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
     console.log("title was clicked!");
-    title.style.color = "blue";
+    h1.style.color = "blue";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+    alert("Copier!");
+}
+function handleWindowOffline() {
+    alert("no wifi");
+}
+function handleWindowOnline() {
+    alert("!!--wifi--!!");
 }
 
 // title.onclick = handleTitleClick; 와 동일
 // addEventListener를 사용할 경우, removeEventListener로 삭제도 가능
-title.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
