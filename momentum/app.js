937665d9a1f51b4157c37cfdb0c60ba0df4d9b1e
Jeck-Lee <jecklee02@gmail.com>
2022-12-16 19:08:48 +0900

    submit event & event argument & preventDefault

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,10 @@
 const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-
-function onLoginBtnClick(){
-    alert("Hello " + username);
+function onLoginSubmit(info) {
+    // 이벤트의 기본 행동이 발생되지 않도록 막는 함수 -> 새로고침 안 됨
+    info.preventDefault();
+    console.log(info);
 }
-loginButton.addEventListener("click",onLoginBtnClick);
+
+// enter를 누르거나 submit 클릭 시 발생
+loginForm.addEventListener("submit", onLoginSubmit);
