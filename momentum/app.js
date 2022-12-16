d3d104b7e308e7e9274990f8ce1c1672cb326f8c
Jeck-Lee <jecklee02@gmail.com>
2022-12-16 19:45:55 +0900

    localStorage.setItem

--- momentum/app.js
+++ momentum/app.js
@@ -4,6 +4,8 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
     // 이벤트의 기본 행동이 발생되지 않도록 막는 함수 -> 새로고침 안 됨
     event.preventDefault();
@@ -13,7 +15,10 @@ function onLoginSubmit(event) {
     // warning: single or double quatation 아님
     greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(USERNAME_KEY, username);
 }
+
 
 // enter를 누르거나 submit 클릭 시 발생
 loginForm.addEventListener("submit", onLoginSubmit);
