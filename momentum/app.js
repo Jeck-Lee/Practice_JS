268872643c97c43c84fc9ea68966fd7d91f2acc5
Jeck-Lee <jecklee02@gmail.com>
2022-12-16 19:29:54 +0900

    Hello ${user}!

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,18 @@
 const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(info) {
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
     // 이벤트의 기본 행동이 발생되지 않도록 막는 함수 -> 새로고침 안 됨
-    info.preventDefault();
-    console.log(info);
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText = "Hello, " + username;
+    // warning: single or double quatation 아님
+    greeting.innerText = `Hello, ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 // enter를 누르거나 submit 클릭 시 발생
