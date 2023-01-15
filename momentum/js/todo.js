ebdc970dfcd18699ebbdc6752560ecd9fb1e77e1
Jeck-Lee <jecklee02@gmail.com>
2023-01-15 11:43:27 +0900

    Save toDos

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    // toDos를 그냥 저장하면 "a,b,c"의 텍스트로 저장됨
+    // stringify를 사용할 시, ["a","b","c"]의 형태로 저장됨
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     toDoList.removeChild(li);
@@ -23,6 +31,8 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
+    saveToDos();
     paintToDo(newTodo);
 }
 
